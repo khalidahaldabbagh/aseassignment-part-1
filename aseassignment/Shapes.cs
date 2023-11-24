69329ddb4a241d029e479b66c2db471f1faa8047
khalidahaldabbagh <147397546+khalidahaldabbagh@users.noreply.github.com>
2023-11-24 01:26:14 +0000

    Shapes interface defines an interface called Shapes with a method draw that expects a Graphics object as a parameter.

---
+++ aseassignment/Shapes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aseassignment
+{
+    /// <summary>
+    /// The main entry point for the application. The interface for different types of shapes.
+    /// </summary>
+    interface Shapes
+    {
+        /// <summary>
+        /// The method that draws the shape.
+        /// </summary>
+        /// <param name="g"></param>
+        void draw(Graphics g);
+    }
+}
